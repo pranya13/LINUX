eae6c3fb1153afd18d100bbb79b42415effc32e5
pranya13 <143314085+pranya13@users.noreply.github.com>
2023-12-03 15:30:27 +0530

    Add files via upload

---
+++ lab-01.sh
@@ -0,0 +1,15 @@
+function cal(){
+var1=26
+var2=13
+((add=var1+var2))
+((sub=var1-var2))
+((pro=var1*var2))
+((div=var1/var2))
+}
+cal
+
+echo $add
+echo $sub
+echo $pro
+echo $div
+
---
+++ lab01.sh
@@ -0,0 +1,5 @@
+name=pranya
+if [$name=pranya]; then
+echo "her name is pranya."
+fi
+
